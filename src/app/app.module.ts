682c13bd113367908f3a7a15d747e8672df99509
guyAriol <software@vihautech.cm>
2019-03-12 22:06:37 +0100

    firebase intregrated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+
+import {AngularFireModule} from 'angularfire2/';
+import {AngularFireDatabaseModule} from 'angularfire2/database';
+import {AngularFireAuthModule} from 'angularfire2/auth';
+
 import { SplashScreen } from '@ionic-native/splash-screen';
-import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCfhQIXhUXY1FQOov_pQk63G4frcZe5HBk",
+  authDomain: "isy-event.firebaseapp.com",
+  databaseURL: "https://isy-event.firebaseio.com",
+  projectId: "isy-event",
+  storageBucket: "isy-event.appspot.com",
+  messagingSenderId: "320935505950"
+};
 
 @NgModule({
   declarations: [
@@ -14,7 +27,11 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireAuthModule,
+    AngularFireDatabaseModule
+
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -22,7 +39,6 @@ import { HomePage } from '../pages/home/home';
     HomePage
   ],
   providers: [
-    StatusBar,
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
