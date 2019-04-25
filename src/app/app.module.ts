73d1d8234065aab6469ac17f55f048bfb6d15e9a
guyAriol <software@vihautech.cm>
2019-04-25 21:49:57 +0200

    custom fonts added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { IonicStorageModule } from '@ionic/storage';
+
+import { AngularFireModule } from 'angularfire2/';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
@@ -14,6 +20,15 @@ import { StorageProvider } from '../providers/storage/storage';
 import { SubscriptionProvider } from '../providers/subscription/subscription';
 import { UserProvider } from '../providers/user/user';
 
+const firebaseConfig = {
+  apiKey: "AIzaSyCfhQIXhUXY1FQOov_pQk63G4frcZe5HBk",
+  authDomain: "isy-event.firebaseapp.com",
+  databaseURL: "https://isy-event.firebaseio.com",
+  projectId: "isy-event",
+  storageBucket: "isy-event.appspot.com",
+  messagingSenderId: "320935505950"
+};
+
 @NgModule({
   declarations: [
     MyApp,
@@ -21,7 +36,11 @@ import { UserProvider } from '../providers/user/user';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    IonicStorageModule.forRoot(),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireAuthModule,
+    AngularFireDatabaseModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
