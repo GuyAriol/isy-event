3eb847d032c6b45dba9eb112e17107ade2640b26
guyAriol <software@vihautech.cm>
2019-05-01 09:17:48 +0200

    clean up ChatController class. introduce cordova hook

--- src/pages/input/input.ts
+++ src/pages/input/input.ts
@@ -2,8 +2,6 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { DeviceProvider } from '../../providers/device/device';
 import { BluetoothProvider } from '../../providers/ble/bluetooth';
-import { Broadcaster } from '@ionic-native/broadcaster';
-
 
 
 @IonicPage()
@@ -18,7 +16,6 @@ export class InputPage {
     public navParams: NavParams,
     public deviceProv: DeviceProvider,
     public bluetoothProv: BluetoothProvider,
-    private nativeBroadcast: Broadcaster,
 
 
   ) {
